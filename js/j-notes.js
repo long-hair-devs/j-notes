204352e6ccf5c0f4d30ba362e31283726da1b857
tboerc <tiago.boer@hotmail.com>
2019-04-26 00:44:48 -0300

    Initial build

---
+++ js/j-notes.js
@@ -0,0 +1,14 @@
+//Atributos
+
+//Main
+$(function(){
+    /* Faz a fonte ser dinâmica, igual no css, porém aqui é possivel arredondar o valor, diminui chance de gliches */
+    calcTamanhoFonte();
+    $(window).resize(function() { calcTamanhoFonte() });
+});
+
+//Secundários
+function calcTamanhoFonte() {
+    let tamanho = Math.round(6 + ($(this).width() / 100) * 0.5);
+    $("html").css('font-size', tamanho);
+}
