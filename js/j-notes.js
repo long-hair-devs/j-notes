229a4a5704075157ef76cf4a18c56d4aa0439955
tboerc <tiago.boer@hotmail.com>
2019-04-27 01:11:24 -0300

    CSS Improvements

--- js/j-notes.js
+++ js/j-notes.js
@@ -5,6 +5,19 @@ $(function () {
     /* Faz a fonte ser dinâmica, igual no css, porém aqui é possivel arredondar o valor, diminui chance de gliches */
     calcTamanhoFonte();
     $(window).resize(function () { calcTamanhoFonte() });
+
+    /* Listener para abrir o menu principal */
+    $("#icone-menu").click(function () {
+        $(".menu-movel").addClass("menu-movel--animacao");
+        if (!$(".menu-movel").hasClass("menu-movel--visivel")) {
+            $(".menu-movel").addClass("menu-movel--visivel");
+        } else {
+            $(".menu-movel").removeClass("menu-movel--visivel");
+        }
+        $(".menu-movel").one("transitionend", function (e) {
+            $(".menu-movel").removeClass("menu-movel--animacao");
+        });
+    });
 });
 
 //Secundários
