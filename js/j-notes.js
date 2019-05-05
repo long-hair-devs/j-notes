34b023a294d3235d8f22ce72ffa940942669d988
tboerc <tiago.boer@hotmail.com>
2019-05-05 20:52:37 -0300

    More stuff on calendar, started complete task

--- js/j-notes.js
+++ js/j-notes.js
@@ -25,6 +25,16 @@ $(function () {
     /*--- Calendário ---*/
     /* Inicia o calendário após a pagina ser carregada */
     mostrarCalendario(mesAtual, anoAtual);
+
+    /* Listeners para quando clicar nos botões de voltar e avançar mês */
+    $("#volta-mes").click(function () { voltaMes() });
+    $("#avanca-mes").click(function () { avancaMes() });
+
+    /* Listener para mostrar a lista de anos */
+    $("#mostra-lista-ano").click(function () { abreFechaListaAnos() });
+
+    /* Listener para mudar o ano, e voltar a lista de anos */
+    $(".box-lista-anos").on("click", "span", function () { mudaAno($(this)); abreFechaListaAnos() });
 });
 
 //Secundários
@@ -50,10 +60,20 @@ function ativaScrollSpy() {
     $("#menu-fixo").unbind();
     $("#menu-movel").unbind();
     if ($(window).width() <= 720) {
-        $("#menu-movel").scrollspy({ offset: -5.5 * parseInt($("html").css('font-size')) });
+        $("#menu-movel").scrollspy({
+            offset: -5.5 * parseInt($("html").css('font-size')),
+            animate: true,
+            duration: 400
+        });
     } else {
-        $("#menu-fixo").scrollspy();
-        $("#menu-movel").scrollspy();
+        $("#menu-fixo").scrollspy({
+            animate: true,
+            duration: 400
+        });
+        $("#menu-movel").scrollspy({
+            animate: true,
+            duration: 400
+        });
     }
 }
 
@@ -62,13 +82,15 @@ function mostrarCalendario(mes, ano) {
     let primeiroDia = new Date(ano, mes).getDay();
     let totalDiasMes = new Date(ano, mes + 1, 0).getDate();
     $("#corpo-calendario").empty();
-    // $("#mes").text(meses[mes]);
-    // $("#ano").text(ano);
+    $("#mes").text(meses[mes]);
+    $("#ano").text(ano);
     let data = 1;
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 6; i++) {
         let tabela = "<tr>";
         for (let j = 0; j < 7; j++) {
-            if (i === 0 && j < primeiroDia) {
+            if ((i === 0 && j < primeiroDia) || (i === 5 && data > totalDiasMes)) {
+                //caso esteja na primeira semana e o dia for menor que o primeiro dia, colocar td vazio
+                //caso esteja na ultima semana, e o mês já tiver acabado, coloca o td vazio para dar o tamanho certo
                 tabela += "<td></td>";
             } else if (data > totalDiasMes) {
                 break;
@@ -84,4 +106,38 @@ function mostrarCalendario(mes, ano) {
         tabela += "</tr>"
         $("#corpo-calendario").append(tabela);
     }
+}
+
+function avancaMes() {
+    anoAtual = (mesAtual === 11) ? anoAtual + 1 : anoAtual;
+    mesAtual = (mesAtual + 1) % 12;
+    mostrarCalendario(mesAtual, anoAtual);
+}
+
+function voltaMes() {
+    anoAtual = (mesAtual === 0) ? anoAtual - 1 : anoAtual;
+    mesAtual = (mesAtual === 0) ? 11 : mesAtual - 1;
+    mostrarCalendario(mesAtual, anoAtual);
+}
+
+function abreFechaListaAnos() {
+    $(".wrapper-lista-anos").addClass("wrapper-lista-anos--animacao");
+    $("#mostra-lista-ano").addClass("ano--animacao");
+
+    if (!$(".wrapper-lista-anos").hasClass("wrapper-lista-anos--visivel")) {
+        $(".wrapper-lista-anos").addClass("wrapper-lista-anos--visivel");
+        $("#mostra-lista-ano").css('transform', 'rotate(-180deg)');
+    } else {
+        $(".wrapper-lista-anos").removeClass("wrapper-lista-anos--visivel");
+        $("#mostra-lista-ano").css('transform', 'none');
+    }
+    $(".wrapper-lista-anos").one("transitionend", function (e) {
+        $(".wrapper-lista-anos").removeClass("wrapper-lista-anos--animacao");
+        $("#mostra-lista-ano").removeClass("ano--animacao");
+    });
+}
+
+function mudaAno(ano) {
+    anoAtual = parseInt($(ano).text());
+    mostrarCalendario(mesAtual, anoAtual);
 }
