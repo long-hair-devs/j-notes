a5c0f11220c5cc4824355e46089522dbbbdac852
Vitor Luis Fernandes <vitor-l-f@hotmail.com>
2019-05-15 21:06:25 -0300

    add data base jbanco
    first upload of data base

---
+++ jbanco.sql
@@ -0,0 +1,40 @@
+drop database jbanco;
+create database jbanco;
+use jbanco;
+
+CREATE TABLE IF NOT EXISTS usuarios (
+  id_user int(11) NOT NULL AUTO_INCREMENT ,
+  usuario varchar(100),
+  email varchar(100),
+  senha varchar(100),
+  
+  PRIMARY KEY (id_user)
+);
+
+CREATE TABLE IF NOT EXISTS cliente (
+  id_cliente int(11) NOT NULL AUTO_INCREMENT ,
+  nome varchar(50),
+  telefone1 varchar(15),
+  telefone2 varchar(15),
+  endereco varchar(245),
+  
+  PRIMARY KEY (id_cliente)
+);
+
+CREATE TABLE IF NOT EXISTS tarefas (
+  id_tarefa int(11) NOT NULL AUTO_INCREMENT ,
+  id_user int(11),
+  id_cliente int(11),
+  
+  dia date,
+  periodo varchar(5),
+  problema varchar(245),
+  informacoes varchar(245),
+  total_recebido varchar(10),
+  total_gasto varchar(10),
+  observacoes_servico varchar(245),
+  
+  FOREIGN KEY (id_user) REFERENCES usuarios(id_user),
+  FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+  PRIMARY KEY (id_tarefa)
+);
