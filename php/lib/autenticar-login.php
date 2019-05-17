70dfbc87988902db384882d0a88da304fda1e761
vlfernandes <vitor-l-f@hotmail.com>
2019-05-17 16:30:47 -0300

    Some fixes on database connection

--- php/lib/autenticar-login.php
+++ php/lib/autenticar-login.php
@@ -1,24 +1,22 @@
 <?php
-    include_once("conexao.php");
+include_once("conexao.php");
 
-    $_usuario = $_POST['nome'];
-    $_senha = $_POST['senha'];
-    $_senha = md5($_senha);
+$_usuario = $_POST['nome'];
+$_senha = md5($_POST['senha']);
 
-    $_sql = "SELECT id_user FROM usuarios WHERE usuario = '$_usuario' and senha = '$_senha'";
-    $_query = mysqli_query($_conexao, $_sql) or die (mysqli_error());
-    $_id = mysqli_fetch_array($_query);
-    $_rows = mysqli_num_rows($_query);
+$_sql = "SELECT id_user FROM usuarios WHERE usuario = '$_usuario' and senha = '$_senha'";
+$_query = mysqli_query($_conexao, $_sql);
+$_id = mysqli_fetch_array($_query);
+$_rows = mysqli_num_rows($_query);
 
-    mysqli_close($_conexao);
+mysqli_close($_conexao);
 
 
-    if($_rows > 0){
-        session_start();
-        $_SESSION['nome'] = $_usuario;
-        $_SESSION['id'] = $_id[0];
-        header("location:../j-notes.php");
-    }else{
-        header("location:../login.php");
-    }
-?>
+if ($_rows > 0) {
+    session_start();
+    $_SESSION['nome'] = $_usuario;
+    $_SESSION['id'] = $_id[0];
+    header("location:../j-notes.php");
+} else {
+    header("location:../login.php");
+}
--- php/lib/cadastrar-user.php
+++ php/lib/cadastrar-user.php
@@ -1,89 +1,61 @@
 <?php
-    include_once("conexao.php");
+include_once("conexao.php");
 
-    if (isset($_POST['user_check'])) {
-        $_usuario = $_POST['user'];
-        $_sql = "SELECT * FROM usuarios WHERE usuario ='$_usuario'";
-        $_query = mysqli_query($_conexao, $_sql) or die (mysqli_error());
-        $_rows = mysqli_num_rows($_query);
-        if ($_rows > 0) {
-          echo 'taken';	
-        }else{
-          echo 'not_taken';
-        }
-        exit();
-    }
-
-    if (isset($_POST['mail_check'])) {
-      $_mail = $_POST['mail'];
-      $_sql = "SELECT * FROM usuarios WHERE email ='$_mail'";
-      $_query = mysqli_query($_conexao, $_sql) or die (mysqli_error());
-      $_rows = mysqli_num_rows($_query);
-      if ($_rows > 0) {
-        echo 'taken';	
-      }else{
-        echo 'not_taken';
-      }
-      exit();
+if (isset($_POST['user_check'])) {
+  $_usuario = $_POST['user'];
+  $_sql = "SELECT * FROM usuarios WHERE usuario ='$_usuario'";
+  $_query = mysqli_query($_conexao, $_sql);
+  $_rows = mysqli_num_rows($_query);
+  if ($_rows > 0) {
+    echo 'taken';
+  } else {
+    echo 'not_taken';
   }
+  exit();
+}
 
-  if (isset($_POST['save'])) {
-  	$_user = $_POST['user'];
-  	$_mail = $_POST['mail'];
-    $_senha = md5($_POST['senha']);
+if (isset($_POST['mail_check'])) {
+  $_mail = $_POST['mail'];
+  $_sql = "SELECT * FROM usuarios WHERE email ='$_mail'";
+  $_query = mysqli_query($_conexao, $_sql);
+  $_rows = mysqli_num_rows($_query);
+  if ($_rows > 0) {
+    echo 'taken';
+  } else {
+    echo 'not_taken';
+  }
+  exit();
+}
 
-    //verifica se o usuario esta disponivel
-    $_sql = "SELECT * FROM usuarios WHERE usuario = '$_user'";
-    $_query = mysqli_query($_conexao, $_sql) or die (mysqli_error());
-    if(mysqli_num_rows($_query) > 0){
-      echo("Nome de usuario não está disponível");
-      exit();
-    }
+if (isset($_POST['save'])) {
+  $_user = $_POST['user'];
+  $_mail = $_POST['mail'];
+  $_senha = md5($_POST['senha']);
 
-    //Verifica se o email é valido
-    if(strpos($_mail, '@') == NULL){
-      echo("Email inválido");
-      exit();
-    }
-
-    //verifica se o email esta disponivel
-    $_sql = "SELECT * FROM usuarios WHERE email = '$_mail'";
-    $_query = mysqli_query($_conexao, $_sql) or die (mysqli_error());
-    if(mysqli_num_rows($_query) > 0){
-      echo("Email ja está sendo utilizado");
-      exit();
-    }
-
-    $_sql = "INSERT INTO usuarios(usuario, email, senha) VALUES ('$_user', '$_mail', '$_senha')";
-    mysqli_query($_conexao, $_sql) or die (mysqli_error());
-    echo("sucesso");
+  //verifica se o usuario esta disponivel
+  $_sql = "SELECT * FROM usuarios WHERE usuario = '$_user'";
+  $_query = mysqli_query($_conexao, $_sql);
+  if (mysqli_num_rows($_query) > 0) {
+    echo ("Nome de usuario não está disponível");
     exit();
   }
-    
-    // $_usuario = $_POST['user'];
-    // $_email = $_POST['mail'];
-    // $_senha = $_POST['senha-verificada'];
-    
-    // $_senha = md5($_senha);
 
-    // $_sql = "SELECT * FROM usuarios WHERE usuario = '$_usuario'";
-    // $_query = mysqli_query($_conexao, $_sql) or die (mysqli_error());
-    // $_rows = mysqli_num_rows($_query);
+  //Verifica se o email é valido
+  if (strpos($_mail, '@') == NULL) {
+    echo ("Email inválido");
+    exit();
+  }
 
-    // if($_rows > 0){
-    //     header("location:../login.php#");
-    // }else{
-    //     $_sql = "INSERT INTO usuarios(usuario, email, senha) VALUES ('$_usuario', '$_email', '$_senha')";
+  //verifica se o email esta disponivel
+  $_sql = "SELECT * FROM usuarios WHERE email = '$_mail'";
+  $_query = mysqli_query($_conexao, $_sql);
+  if (mysqli_num_rows($_query) > 0) {
+    echo ("Email ja está sendo utilizado");
+    exit();
+  }
 
-    //     $_query = mysqli_query($_conexao, $_sql) or die (mysqli_error());
-
-    //     if ($_query) {
-    //         header("location:../login.php");
-    //     } else {
-    //         header("location:../../index.html");
-    //     }
-    // }
-
-    
-    
-?>
+  $_sql = "INSERT INTO usuarios(usuario, email, senha) VALUES ('$_user', '$_mail', '$_senha')";
+  mysqli_query($_conexao, $_sql);
+  echo ("sucesso");
+  exit();
+}
