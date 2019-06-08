ada9eea07ea0cb7172bd837cb0d1e06b5888a949
tboerc <tiago.boer@hotmail.com>
2019-06-08 10:13:48 -0300

    Minor fixes to better optmization

--- php/lib/process-j-notes.php
+++ php/lib/process-j-notes.php
@@ -1,5 +1,5 @@
 <?php
-$db = mysqli_connect('localhost', 'root', '', 'jbanco');
+include_once('conexao.php');
 
 session_start();
 
@@ -12,7 +12,7 @@ $user_id = $_SESSION['id'];
 if (isset($_POST['verifica_tel_no_banco'])) {
     $tel1 = $_POST['tel1'];
     $sql = "SELECT nome, endereco, telefone2 FROM cliente WHERE telefone1='$tel1' AND id_user='$user_id'";
-    $results = mysqli_query($db, $sql) or die($db->error);
+    $results = mysqli_query($_conexao, $sql) or die($_conexao->error);
 
     if (mysqli_num_rows($results) > 0) {
         $results = mysqli_fetch_array($results, MYSQLI_NUM);
@@ -30,7 +30,7 @@ if (isset($_POST['aplica-update-no-cliente'])) {
     $tel2 = $_POST['tel2'];
 
     $sql = "UPDATE cliente SET nome = '$nome', endereco = '$endereco', telefone2 = '$tel2' WHERE telefone1='$tel1' AND id_user='$user_id'";
-    mysqli_query($db, $sql) or die($db->error);
+    mysqli_query($_conexao, $sql) or die($_conexao->error);
     exit();
 }
 
@@ -42,7 +42,7 @@ if (isset($_POST['cadastra-novo-cliente'])) {
 
     $sql = "INSERT INTO cliente (id_user, nome, endereco, telefone1, telefone2) 
                     VALUES ('$user_id', '$nome', '$endereco', '$tel1', '$tel2')";
-    mysqli_query($db, $sql) or die($db->error);
+    mysqli_query($_conexao, $sql) or die($_conexao->error);
     exit();
 }
 
@@ -58,7 +58,7 @@ if (isset($_POST['cadastra-nova-tarefa'])) {
 
     $sql = "INSERT INTO tarefas (telefone1, id_user, nome, endereco, telefone2, dia, periodo, problema, informacoes) 
                     VALUES ('$tel1', '$user_id', '$nome', '$endereco', '$tel2', str_to_date('$data', '%d/%m/%Y'), '$periodo', '$problema', '$infoAdicional')";
-    mysqli_query($db, $sql) or die($db->error);
+    mysqli_query($_conexao, $sql) or die($_conexao->error);
     exit();
 }
 
@@ -68,14 +68,14 @@ if (isset($_POST['pega-eventos-mes'])) {
 
     $sql = "SELECT id_tarefa, dia, periodo, problema, informacoes, nome, telefone1, telefone2, endereco, total_recebido, total_gasto, observacoes_servico 
                     FROM tarefas WHERE MONTH(dia) = '$mes' AND YEAR(dia) = '$ano' AND id_user ='$user_id'";
-    $results = mysqli_query($db, $sql) or die($db->error);
+    $results = mysqli_query($_conexao, $sql) or die($_conexao->error);
 
     if (mysqli_num_rows($results) > 0) {
         $results = mysqli_fetch_all($results, MYSQLI_NUM);
         echo json_encode($results);
-        exit();
+    } else {
+        echo 0;
     }
-    echo 0;
     exit();
 }
 
@@ -92,8 +92,7 @@ if (isset($_POST['atualiza-tarefa'])) {
 
     $sql = "UPDATE tarefas SET nome='$nome', endereco='$endereco', telefone2='$tel2', dia=str_to_date('$data', '%d/%m/%Y'), periodo='$periodo', problema='$problema', informacoes='$infoAdicional'
                 WHERE id_tarefa='$id' AND id_user ='$user_id'";
-
-    mysqli_query($db, $sql) or die($db->error);
+    mysqli_query($_conexao, $sql) or die($_conexao->error);
     exit();
 }
 
@@ -102,11 +101,11 @@ if (isset($_POST['deleta-tarefa'])) {
 
     if (is_numeric($id)) {
         $sql = "SELECT * FROM tarefas WHERE id_tarefa='$id' AND id_user='$user_id'";
-        $results = mysqli_query($db, $sql) or die($db->error);
+        $results = mysqli_query($_conexao, $sql) or die($_conexao->error);
+
         if (mysqli_num_rows($results) > 0) {
             $sql = "DELETE FROM tarefas WHERE id_tarefa='$id'";
-            mysqli_query($db, $sql) or die($db->error);
-
+            mysqli_query($_conexao, $sql) or die($_conexao->error);
             echo 1;
         }
     }
@@ -118,14 +117,14 @@ if (isset($_POST['pega-nao-concluidas'])) {
 
     $sql = "SELECT id_tarefa, dia, periodo, problema, informacoes, nome, telefone1, telefone2, endereco
                     FROM tarefas WHERE DATE(dia) < '$hoje' AND id_user ='$user_id' AND total_recebido IS NULL";
-    $results = mysqli_query($db, $sql) or die($db->error);
+    $results = mysqli_query($_conexao, $sql) or die($_conexao->error);
 
     if (mysqli_num_rows($results) > 0) {
         $results = mysqli_fetch_all($results, MYSQLI_NUM);
         echo json_encode($results);
-        exit();
+    } else {
+        echo 0;
     }
-    echo 0;
     exit();
 }
 
@@ -138,10 +137,8 @@ if (isset($_POST['concluir-tarefa'])) {
     if (is_numeric($id)) {
         $sql = "UPDATE tarefas SET total_recebido='$tRecebido', total_gasto='$tGasto', observacoes_servico='$obs'
                 WHERE id_tarefa='$id' AND id_user ='$user_id'";
-
-        mysqli_query($db, $sql) or die($db->error);
-
-        echo "certo";
+        mysqli_query($_conexao, $sql) or die($_conexao->error);
+        echo 1;
     }
     exit();
 }
